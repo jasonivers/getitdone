d89d9a0dc08cb1c766820e0bbc4354cb7d701088
Jason <jasonivers@gmail.com>
2021-10-15 17:34:30 -0700

    Update routes for root paths

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,13 @@ Rails.application.routes.draw do
   end
   use_doorkeeper
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'projects#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
